7e76575400a0fa6357090f87b24bad6337672301
glenncai <glenncai.work@gmail.com>
2021-09-16 22:48:11 +0800

    feat: add home page for testing

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app');
+@section('content')
+	<div class="text-center">
+		<h1>Hello {{ Auth::user()->name }}</h1>
+	</div>
+@endsection
--- resources/views/test.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.app');
-@section('content')
-	<input type="text" class="form-control" />
-	<button class="btn btn-primary">Submit</button>
-@endsection
